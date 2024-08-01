43b8dc55da776fbc6f18719eda1f452688b45087
Jessie <jessietajalle08@gmail.com>
2024-08-01 13:27:02 -0700

    Created Doctor class could use revisions down the road

---
+++ ClinicAppScheduler/Models/Doctor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicAppScheduler.Models
+{
+    public class Doctor
+    {
+        // tell EF Core to make this primary key
+        [Key]
+        public int DoctorId { get; set; }
+
+        // legal first and last name
+        [Required]
+        public string FullName { get; set; }
+
+        // medical license number
+        [Required]
+        public string LicenseNumber { get; set; }
+
+        // work email address
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
