0eb91c663112910aa4414a6c45fcc3452ba6f094
Jessie <jessietajalle08@gmail.com>
2024-08-01 12:47:19 -0700

    Created ClinicContext and established connection string to create initial database. Also implemented DbSet<Patient> to create Patient table. Still need to make InitialCreate

---
+++ ClinicAppScheduler/Data/ClinicContext.cs
@@ -0,0 +1,29 @@
+﻿using ClinicAppScheduler.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicAppScheduler.Data
+{
+    internal class ClinicContext : DbContext
+    {
+         public ClinicContext() 
+         {
+
+         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Database = the desired name for database
+            // Server = the server we are connecting too. LocalDB is included with VS
+            // Trusted_Connection - indicated that our windows account should be used
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Clinic;Trusted_Connection=True;");
+        }
+
+        // DbSet for Patients
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
