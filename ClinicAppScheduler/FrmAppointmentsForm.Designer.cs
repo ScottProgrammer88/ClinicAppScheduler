873db49ec5e0efd6c4e025e4386a516038bc3f3c
Jessie <jessietajalle08@gmail.com>
2024-08-19 23:39:04 -0700

    Revamped appointment form design

--- ClinicAppScheduler/FrmAppointmentsForm.Designer.cs
+++ ClinicAppScheduler/FrmAppointmentsForm.Designer.cs
@@ -29,10 +29,7 @@
         private void InitializeComponent()
         {
             lblPatientID = new Label();
-            lblDoctorID = new Label();
             lblAppointmentDate = new Label();
-            txtPatientID = new TextBox();
-            txtDoctorID = new TextBox();
             btnSave = new Button();
             dtpAppointmentDate = new DateTimePicker();
             lblAppointmentTime = new Label();
@@ -43,63 +40,43 @@
             bookToolStripMenuItem = new ToolStripMenuItem();
             manageToolStripMenuItem = new ToolStripMenuItem();
             historyToolStripMenuItem = new ToolStripMenuItem();
+            listBox1 = new ListBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // lblPatientID
             // 
             lblPatientID.AutoSize = true;
-            lblPatientID.Location = new Point(97, 60);
+            lblPatientID.Location = new Point(69, 46);
             lblPatientID.Name = "lblPatientID";
-            lblPatientID.Size = new Size(58, 15);
+            lblPatientID.Size = new Size(97, 15);
             lblPatientID.TabIndex = 0;
-            lblPatientID.Text = "Patient ID";
-            // 
-            // lblDoctorID
-            // 
-            lblDoctorID.AutoSize = true;
-            lblDoctorID.Location = new Point(97, 123);
-            lblDoctorID.Name = "lblDoctorID";
-            lblDoctorID.Size = new Size(57, 15);
-            lblDoctorID.TabIndex = 1;
-            lblDoctorID.Text = "Doctor ID";
+            lblPatientID.Text = "Choose A Doctor";
             // 
             // lblAppointmentDate
             // 
             lblAppointmentDate.AutoSize = true;
-            lblAppointmentDate.Location = new Point(97, 186);
+            lblAppointmentDate.Location = new Point(225, 103);
             lblAppointmentDate.Name = "lblAppointmentDate";
             lblAppointmentDate.Size = new Size(105, 15);
             lblAppointmentDate.TabIndex = 2;
             lblAppointmentDate.Text = "Appointment Date";
             // 
-            // txtPatientID
-            // 
-            txtPatientID.Location = new Point(234, 60);
-            txtPatientID.Name = "txtPatientID";
-            txtPatientID.Size = new Size(186, 23);
-            txtPatientID.TabIndex = 3;
-            // 
-            // txtDoctorID
-            // 
-            txtDoctorID.Location = new Point(234, 123);
-            txtDoctorID.Name = "txtDoctorID";
-            txtDoctorID.Size = new Size(186, 23);
-            txtDoctorID.TabIndex = 4;
-            // 
             // btnSave
             // 
-            btnSave.Location = new Point(80, 324);
+            btnSave.Location = new Point(515, 248);
             btnSave.Name = "btnSave";
-            btnSave.Size = new Size(122, 46);
+            btnSave.Size = new Size(99, 30);
             btnSave.TabIndex = 8;
-            btnSave.Text = "Save";
+            btnSave.Text = "Submit";
             btnSave.UseVisualStyleBackColor = true;
             btnSave.Click += btnSave_Click;
             // 
             // dtpAppointmentDate
             // 
-            dtpAppointmentDate.Location = new Point(234, 186);
+            dtpAppointmentDate.Location = new Point(361, 97);
             dtpAppointmentDate.Name = "dtpAppointmentDate";
             dtpAppointmentDate.Size = new Size(253, 23);
             dtpAppointmentDate.TabIndex = 9;
@@ -107,7 +84,7 @@
             // lblAppointmentTime
             // 
             lblAppointmentTime.AutoSize = true;
-            lblAppointmentTime.Location = new Point(97, 254);
+            lblAppointmentTime.Location = new Point(225, 153);
             lblAppointmentTime.Name = "lblAppointmentTime";
             lblAppointmentTime.Size = new Size(107, 15);
             lblAppointmentTime.TabIndex = 10;
@@ -116,7 +93,7 @@
             // dtpAppointmentTime
             // 
             dtpAppointmentTime.Format = DateTimePickerFormat.Time;
-            dtpAppointmentTime.Location = new Point(234, 254);
+            dtpAppointmentTime.Location = new Point(361, 147);
             dtpAppointmentTime.Name = "dtpAppointmentTime";
             dtpAppointmentTime.ShowUpDown = true;
             dtpAppointmentTime.Size = new Size(253, 23);
@@ -129,7 +106,7 @@
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Padding = new Padding(4, 1, 0, 1);
-            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.Size = new Size(687, 24);
             menuStrip1.TabIndex = 12;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -150,36 +127,67 @@
             // bookToolStripMenuItem
             // 
             bookToolStripMenuItem.Name = "bookToolStripMenuItem";
-            bookToolStripMenuItem.Size = new Size(180, 22);
+            bookToolStripMenuItem.Size = new Size(117, 22);
             bookToolStripMenuItem.Text = "Book";
             // 
             // manageToolStripMenuItem
             // 
             manageToolStripMenuItem.Name = "manageToolStripMenuItem";
-            manageToolStripMenuItem.Size = new Size(180, 22);
+            manageToolStripMenuItem.Size = new Size(117, 22);
             manageToolStripMenuItem.Text = "Manage";
             manageToolStripMenuItem.Click += manageToolStripMenuItem_Click;
             // 
             // historyToolStripMenuItem
             // 
             historyToolStripMenuItem.Name = "historyToolStripMenuItem";
-            historyToolStripMenuItem.Size = new Size(180, 22);
+            historyToolStripMenuItem.Size = new Size(117, 22);
             historyToolStripMenuItem.Text = "History";
             historyToolStripMenuItem.Click += historyToolStripMenuItem_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(59, 73);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 154);
+            listBox1.TabIndex = 13;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(46, 259);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 19);
+            radioButton1.TabIndex = 14;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Female";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(154, 259);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(51, 19);
+            radioButton2.TabIndex = 15;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Male";
+            radioButton2.UseVisualStyleBackColor = true;
             // 
             // FrmAppointmentsForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(687, 341);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(listBox1);
             Controls.Add(dtpAppointmentTime);
             Controls.Add(lblAppointmentTime);
             Controls.Add(dtpAppointmentDate);
             Controls.Add(btnSave);
-            Controls.Add(txtDoctorID);
-            Controls.Add(txtPatientID);
             Controls.Add(lblAppointmentDate);
-            Controls.Add(lblDoctorID);
             Controls.Add(lblPatientID);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
@@ -195,10 +203,7 @@
         #endregion
 
         private Label lblPatientID;
-        private Label lblDoctorID;
         private Label lblAppointmentDate;
-        private TextBox txtPatientID;
-        private TextBox txtDoctorID;
         private Button btnSave;
         private DateTimePicker dtpAppointmentDate;
         private Label lblAppointmentTime;
@@ -209,5 +214,8 @@
         private ToolStripMenuItem bookToolStripMenuItem;
         private ToolStripMenuItem manageToolStripMenuItem;
         private ToolStripMenuItem historyToolStripMenuItem;
+        private ListBox listBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
     }
 }
