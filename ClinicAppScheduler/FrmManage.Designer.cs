38abe485dc60a1b445bf1a4572c0581f7f8210e3
Jessie <jessietajalle08@gmail.com>
2024-08-21 21:55:30 -0700

    Users appointment details will be shown in list box, date time picker, time box, and radio button. These controls are disabled

--- ClinicAppScheduler/FrmManage.Designer.cs
+++ ClinicAppScheduler/FrmManage.Designer.cs
@@ -34,6 +34,17 @@
             bookToolStripMenuItem = new ToolStripMenuItem();
             manageToolStripMenuItem = new ToolStripMenuItem();
             historyToolStripMenuItem = new ToolStripMenuItem();
+            ltbDoctors = new ListBox();
+            rdbFemale = new RadioButton();
+            rdbMale = new RadioButton();
+            label1 = new Label();
+            dtpAppointmentDate = new DateTimePicker();
+            lblAppointmentDate = new Label();
+            lblAppointmentTime = new Label();
+            cmbTime = new ComboBox();
+            btnCancel = new Button();
+            btnUpdate = new Button();
+            btnSave = new Button();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -44,7 +55,7 @@
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Padding = new Padding(4, 1, 0, 1);
-            menuStrip1.Size = new Size(560, 24);
+            menuStrip1.Size = new Size(646, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -65,34 +76,150 @@
             // bookToolStripMenuItem
             // 
             bookToolStripMenuItem.Name = "bookToolStripMenuItem";
-            bookToolStripMenuItem.Size = new Size(180, 22);
+            bookToolStripMenuItem.Size = new Size(117, 22);
             bookToolStripMenuItem.Text = "Book";
             bookToolStripMenuItem.Click += bookToolStripMenuItem_Click;
             // 
             // manageToolStripMenuItem
             // 
             manageToolStripMenuItem.Name = "manageToolStripMenuItem";
-            manageToolStripMenuItem.Size = new Size(180, 22);
+            manageToolStripMenuItem.Size = new Size(117, 22);
             manageToolStripMenuItem.Text = "Manage";
             manageToolStripMenuItem.Click += manageToolStripMenuItem_Click;
             // 
             // historyToolStripMenuItem
             // 
             historyToolStripMenuItem.Name = "historyToolStripMenuItem";
-            historyToolStripMenuItem.Size = new Size(180, 22);
+            historyToolStripMenuItem.Size = new Size(117, 22);
             historyToolStripMenuItem.Text = "History";
             historyToolStripMenuItem.Click += historyToolStripMenuItem_Click;
+            // 
+            // ltbDoctors
+            // 
+            ltbDoctors.FormattingEnabled = true;
+            ltbDoctors.ItemHeight = 15;
+            ltbDoctors.Location = new Point(66, 61);
+            ltbDoctors.Name = "ltbDoctors";
+            ltbDoctors.Size = new Size(120, 154);
+            ltbDoctors.TabIndex = 1;
+            // 
+            // rdbFemale
+            // 
+            rdbFemale.AutoSize = true;
+            rdbFemale.Enabled = false;
+            rdbFemale.Location = new Point(44, 247);
+            rdbFemale.Name = "rdbFemale";
+            rdbFemale.Size = new Size(63, 19);
+            rdbFemale.TabIndex = 2;
+            rdbFemale.TabStop = true;
+            rdbFemale.Text = "Female";
+            rdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdbMale
+            // 
+            rdbMale.AutoSize = true;
+            rdbMale.Enabled = false;
+            rdbMale.Location = new Point(159, 247);
+            rdbMale.Name = "rdbMale";
+            rdbMale.Size = new Size(51, 19);
+            rdbMale.TabIndex = 3;
+            rdbMale.TabStop = true;
+            rdbMale.Text = "Male";
+            rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(83, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Doctor Chosen";
+            // 
+            // dtpAppointmentDate
+            // 
+            dtpAppointmentDate.Enabled = false;
+            dtpAppointmentDate.Location = new Point(348, 85);
+            dtpAppointmentDate.Name = "dtpAppointmentDate";
+            dtpAppointmentDate.Size = new Size(253, 23);
+            dtpAppointmentDate.TabIndex = 10;
+            // 
+            // lblAppointmentDate
+            // 
+            lblAppointmentDate.AutoSize = true;
+            lblAppointmentDate.Location = new Point(224, 91);
+            lblAppointmentDate.Name = "lblAppointmentDate";
+            lblAppointmentDate.Size = new Size(105, 15);
+            lblAppointmentDate.TabIndex = 11;
+            lblAppointmentDate.Text = "Appointment Date";
+            // 
+            // lblAppointmentTime
+            // 
+            lblAppointmentTime.AutoSize = true;
+            lblAppointmentTime.Location = new Point(224, 137);
+            lblAppointmentTime.Name = "lblAppointmentTime";
+            lblAppointmentTime.Size = new Size(107, 15);
+            lblAppointmentTime.TabIndex = 12;
+            lblAppointmentTime.Text = "Appointment Time";
+            // 
+            // cmbTime
+            // 
+            cmbTime.FormattingEnabled = true;
+            cmbTime.Location = new Point(348, 134);
+            cmbTime.Name = "cmbTime";
+            cmbTime.Size = new Size(108, 23);
+            cmbTime.TabIndex = 17;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(304, 239);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(106, 34);
+            btnCancel.TabIndex = 19;
+            btnCancel.Text = "Cancel ";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(416, 239);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(106, 34);
+            btnUpdate.TabIndex = 18;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click_1;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(528, 239);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(106, 34);
+            btnSave.TabIndex = 20;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
             // 
             // FrmManage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(560, 270);
+            ClientSize = new Size(646, 316);
+            Controls.Add(btnSave);
+            Controls.Add(btnCancel);
+            Controls.Add(btnUpdate);
+            Controls.Add(cmbTime);
+            Controls.Add(lblAppointmentTime);
+            Controls.Add(lblAppointmentDate);
+            Controls.Add(dtpAppointmentDate);
+            Controls.Add(label1);
+            Controls.Add(rdbMale);
+            Controls.Add(rdbFemale);
+            Controls.Add(ltbDoctors);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Margin = new Padding(2);
             Name = "FrmManage";
             Text = "FrmManage";
+            Load += FrmManage_Load;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
@@ -107,5 +234,16 @@
         private ToolStripMenuItem bookToolStripMenuItem;
         private ToolStripMenuItem manageToolStripMenuItem;
         private ToolStripMenuItem historyToolStripMenuItem;
+        private ListBox ltbDoctors;
+        private RadioButton rdbFemale;
+        private RadioButton rdbMale;
+        private Label label1;
+        private DateTimePicker dtpAppointmentDate;
+        private Label lblAppointmentDate;
+        private Label lblAppointmentTime;
+        private ComboBox cmbTime;
+        private Button btnCancel;
+        private Button btnUpdate;
+        private Button btnSave;
     }
 }
--- ClinicAppScheduler/FrmManage.cs
+++ ClinicAppScheduler/FrmManage.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ClinicAppScheduler.Data;
+using ClinicAppScheduler.Models;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +15,107 @@ namespace ClinicAppScheduler
 {
     public partial class FrmManage : Form
     {
+        private int userId;
+        private Appointment currentAppointment;
+
+
         public FrmManage()
         {
             InitializeComponent();
+            userId = Session.userId;
+            LoadAppointment();
+            DisableControls(true);
         }
+
+        private void FrmManage_Load(object sender, EventArgs e)
+        {
+            // Ensure LoadAppointment is called here if not already called in the constructor
+            // LoadAppointment();
+        }
+
+        private void PopulateComboBox()
+        {
+            cmbTime.Items.Add("09:00 AM");
+            cmbTime.Items.Add("10:00 AM");
+            cmbTime.Items.Add("11:00 AM");
+            cmbTime.Items.Add("12:00 PM");
+            cmbTime.Items.Add("1:00 PM");
+            cmbTime.Items.Add("02:00 PM");
+            cmbTime.Items.Add("03:00 PM");
+            cmbTime.Items.Add("04:00 PM");
+            cmbTime.Items.Add("05:00 PM");
+
+            cmbTime.SelectedIndex = 0;
+        }
+
+        private void LoadAppointment()
+        {
+            using (var context = new ClinicContext())
+            {
+                // Load the current appointment details
+                currentAppointment = context.Appointments
+                    .FirstOrDefault(a => a.PatientId == userId && a.AppointmentDate >= DateTime.Today);
+
+                if (currentAppointment != null)
+                {
+                    // Set the DateTimePicker to the appointment date
+                    dtpAppointmentDate.Value = currentAppointment.AppointmentDate;
+
+                    // Populate the ComboBox with times
+                    PopulateComboBox();
+
+                    // Set the ComboBox to the appointment time
+                    cmbTime.SelectedItem = currentAppointment.AppointmentTime;
+
+                    var doctorDetails = context.Appointments
+                                               .Where(a => a.PatientId == userId)
+                                               .Join(context.Doctors,
+                                                     appointment => appointment.DoctorId,
+                                                     doctor => doctor.DoctorId,
+                                                     (appointment, doctor) => new { doctor.FullName, doctor.Gender })
+                                               .ToList();
+
+                    ltbDoctors.DataSource = doctorDetails.Select(d => d.FullName).ToList();
+
+                    var selectedDoctor = doctorDetails.FirstOrDefault();
+                    if (selectedDoctor != null)
+                    {
+                        ltbDoctors.SelectedItem = selectedDoctor.FullName;
+                        if (selectedDoctor.Gender == 'F')
+                        {
+                            rdbFemale.Checked = true;
+                        }
+                        else if (selectedDoctor.Gender == 'M')
+                        {
+                            rdbMale.Checked = true;
+                        }
+                    }
+                }
+                else
+                {
+                    // user will not be able to do anything
+                    MessageBox.Show("No upcoming appointments found.");
+                    DisableControls(true);
+                    btnCancel.Enabled = false;
+                    btnUpdate.Enabled = false;
+                    btnSave.Enabled = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disables controls so user can not interact with appointment
+        /// </summary>
+        /// <param name="readOnly"></param>
+        private void DisableControls(bool readOnly)
+        {
+            dtpAppointmentDate.Enabled = !readOnly;
+            cmbTime.Enabled = !readOnly;
+            rdbFemale.Enabled = !readOnly;
+            rdbMale.Enabled = !readOnly;
+            btnSave.Enabled = !readOnly;
+        }
+
 
         private void menuToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -41,6 +141,11 @@ namespace ClinicAppScheduler
             FrmHistory history = new FrmHistory();
             history.Show();
             this.Hide();
+        }
+
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            DisableControls(false);
         }
     }
 }
